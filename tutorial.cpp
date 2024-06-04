1bdc1169e222e3c3f0ea4b4c21a4d29859be0cb7
Thomas Ambrogini <thomas.ambrogini@gmail.com>
2024-06-04 16:08:49 +0200

    chore: end of tutorial first step
    *   How to create a basic CMakeLists.txt file and start a project with
        cmake
    *   How to create an executable target
        *   How to add include directories to it
    *   How to access CMake variables inside the C++ source code

---
+++ tutorial.cpp
@@ -0,0 +1,24 @@
+#include "TutorialConfig.h"
+
+#include <cmath>
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+  if (argc < 2) {
+    std::cout << "Version of the program " << Tutorial_VERSION_MAJOR << "." <<
+                    Tutorial_VERSION_MINOR << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    return 1;
+  }
+
+  // convert input to double
+  const double inputValue = std::stod(argv[1]);
+
+  // calculate square root
+  const double outputValue = sqrt(inputValue);
+  std::cout << "The square root of " << inputValue << " is " << outputValue
+            << std::endl;
+  return 0;
+}
